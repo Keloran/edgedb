b0fe7d3a419dd6f650fd1740fae6a1292fb25eb5
Keloran <keloran@develbox.info>
2025-03-22 15:46:17 +0000

    make it easier to test without creating files, and change port

--- go.mod
+++ go.mod
@@ -5,5 +5,8 @@ go 1.21
 require (
 	github.com/bugfixes/go-bugfixes v0.13.0 // indirect
 	github.com/go-logfmt/logfmt v0.6.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
+	github.com/spf13/cobra v1.9.1 // indirect
+	github.com/spf13/pflag v1.0.6 // indirect
 )
--- main.go
+++ main.go
@@ -5,43 +5,63 @@ import (
 	"github.com/bugfixes/go-bugfixes/logs"
 	"github.com/keloran/edgedb/conex"
 	"github.com/keloran/edgedb/sig"
+	"github.com/spf13/cobra"
 	"os"
 	"os/signal"
 	"time"
 )
 
 func main() {
-	port := uint32(8500)
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+	var (
+		port     = uint32(8500)
+		testMode = false
+	)
 
-	logs.Info("Starting...")
+	rootCmd := &cobra.Command{
+		Use: "edgedb",
+		Run: func(cmd *cobra.Command, args []string) {
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
 
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
+			logs.Infof("Listening on port: %d", port)
 
-	go func() {
-		con := conex.NewSystem()
-		con.SetContext(ctx)
-		con.Interval = time.Second * 5
-		con.Port = port
-		conns, err := con.CountConnections()
-		if err != nil {
-			logs.Infof("Error counting the connections: %v", err)
+			signalChan := make(chan os.Signal, 1)
+			signal.Notify(signalChan, os.Interrupt)
+
+			go func() {
+				con := conex.NewSystem()
+				con.SetContext(ctx)
+				con.Interval = time.Second * 5
+				con.Port = port
+				conns, err := con.CountConnections()
+				if err != nil {
+					logs.Infof("Error counting the connections: %v", err)
+					cancel()
+				}
+
+				sigs := sig.NewSystem()
+				sigs.SetContext(ctx)
+				if !testMode {
+					sigs.SetFolder("/tmp/node-exporter")
+				}
+				if err := sigs.SendLogs(conns); err != nil {
+					logs.Infof("Error sending logs: %v", err)
+					cancel()
+				}
+			}()
+			<-signalChan
+
 			cancel()
-		}
+			time.Sleep(time.Second * 5)
+			logs.Info("Shutting down...")
+		},
+	}
 
-		sigs := sig.NewSystem()
-		sigs.SetContext(ctx)
-		//sigs.SetFolder("/tmp/node-exporter")
-		if err := sigs.SendLogs(conns); err != nil {
-			logs.Infof("Error sending logs: %v", err)
-			cancel()
-		}
-	}()
-	<-signalChan
+	rootCmd.Flags().Uint32VarP(&port, "port", "p", 8500, "Port to use for connections")
+	rootCmd.Flags().BoolVarP(&testMode, "test", "t", false, "Run in test mode (don't set folder)")
 
-	cancel()
-	time.Sleep(time.Second * 5)
-	logs.Info("Shutting down...")
+	if err := rootCmd.Execute(); err != nil {
+		logs.Fatal(err)
+		os.Exit(1)
+	}
 }
